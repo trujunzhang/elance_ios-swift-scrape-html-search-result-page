a51dfb21ddaa4e6e4c6cc5bd91373f55da7dfe1a
wanghaogithub720 <wanghaobackup@gmail.com>
2015-05-08 16:21:49 +0800

    xxx

---
+++ UnitTests/FetcherTests/KatPHScrapyFetcherTests.swift
@@ -0,0 +1,29 @@
+//
+//  KatPHScrapyFetcherTests.swift
+//  appBDD
+//
+//  Created by djzhang on 5/8/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class KatPHScrapyFetcherTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testExample() {
+
+    }
+
+
+}
