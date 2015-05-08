7bda0f470d5c222c930a5f0a18e652cac1e86321
wanghaogithub720 <wanghaobackup@gmail.com>
2015-05-08 15:33:25 +0800

    xxx

---
+++ app/app/Business-Logic-Layer/Fetcher/ElanceScrapyFetcher.swift
@@ -0,0 +1,13 @@
+//
+//  ElanceScrapyFetcher.swift
+//  app
+//
+//  Created by djzhang on 5/8/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import Foundation
+
+class ElanceScrapyFetcher: NSObject {
+    
+}
---
+++ app/app/Business-Logic-Layer/Fetcher/KatPHScrapyFetcher.swift
@@ -0,0 +1,13 @@
+//
+//  KatPHScrapyFetcher.swift
+//  app
+//
+//  Created by djzhang on 5/8/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import Foundation
+
+class KatPHScrapyFetcher: NSObject {
+    
+}
