70d1df658e8385fe7bc46c78ae17824e41c4f508
wanghaogithub720 <wanghaobackup@gmail.com>
2015-05-08 15:47:16 +0800

    xxx

---
+++ app/app/Business-Logic-Layer/HtmlParser/ElanceHtmlParser.swift
@@ -0,0 +1,13 @@
+//
+//  ElanceHtmlParser.swift
+//  app
+//
+//  Created by djzhang on 5/8/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import Foundation
+
+class ElanceHtmlParser: NSObject {
+    
+}
