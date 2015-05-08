233f2c1f79521a92ec2682b05d983dddaff089be
wanghaogithub720 <wanghaobackup@gmail.com>
2015-05-08 15:25:43 +0800

    Sample.

---
+++ app/app/SeearchViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app
+//
+//  Created by djzhang on 5/8/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import UIKit
+
+class SeearchViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
