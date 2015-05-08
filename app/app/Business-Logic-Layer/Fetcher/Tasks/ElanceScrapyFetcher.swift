88f378248966cc442583918a12a0a632e36e9352
wanghaogithub720 <wanghaobackup@gmail.com>
2015-05-08 21:29:08 +0800

    Sample.

--- app/app/Business-Logic-Layer/Fetcher/Tasks/ElanceScrapyFetcher.swift
+++ app/app/Business-Logic-Layer/Fetcher/Tasks/ElanceScrapyFetcher.swift
@@ -10,40 +10,39 @@ import Foundation
 
 class ElanceScrapyFetcher: FetchProtocal {
 
-    
-    func fetchHtml(completeHandler: ObjectHandler){
-        let url = "\(elance_host)scrapy"
-        DownloadHtmlHelper.downlaodHtml(url) { (object, error) -> Void in
-            if(error == nil){
-                let html:String = object as! String
+
+    func fetchHtml(completeHandler: ObjectHandler) {
+        let url = "https://www.elance.com/r/jobs/q-scrapy"
+        DownloadHtmlHelper.downlaodHtml(url) {
+            (object, error) -> Void in
+            if (error == nil) {
+                let html: String = object as! String
                 self.parseHtml(html)
-            }else{
+            } else {
                 let x = 0
             }
         }
     }
-    
-    
-    
-    
-    func parseHtml(html:String){
-//        println("html is \(html)")
-        
-        var err : NSError?
-        var parser     = HTMLParser(html: html, error: &err)
+
+
+    func parseHtml(html: String) {
+        println("html is \(html)")
+
+        var err: NSError?
+        var parser = HTMLParser(html: html, error: &err)
         if err != nil {
             println(err)
             exit(1)
         }
-        
-        var bodyNode   = parser.body?.findNodeById("jobSearchResults")
-        
-        if let inputNodes = bodyNode?.findChildTags("div") {
-            for node in inputNodes {
-                println(node.contents)
-            }
-        }
-        
+
+//        var bodyNode = parser.body?.findNodeById("jobSearchResults")
+
+//        if let inputNodes = bodyNode?.findChildClasss("jobCard") {
+//            for node in inputNodes {
+//                println(node.contents)
+//            }
+//        }
+
         //        if let inputNodes = bodyNode?.findChildTags("a") {
         //            for node in inputNodes {
         //                println(node.contents)
@@ -51,5 +50,5 @@ class ElanceScrapyFetcher: FetchProtocal {
         //            }
         //        }
     }
-    
+
 }
