2c9ba9b04f7085c9998b353fbbd3a1e33383d8ed
wanghaogithub720 <wanghaobackup@gmail.com>
2015-05-08 18:36:36 +0800

    xxx

---
+++ UnitTests/FetcherTests/ElanceScrapyFetcherTests.swift
@@ -0,0 +1,46 @@
+//
+//  ElanceScrapyFetcherTests.swift
+//  appBDD
+//
+//  Created by djzhang on 5/8/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ElanceScrapyFetcherTests: XCTestCase {
+    
+    var fetcher: ElanceScrapyFetcher?
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        fetcher = ElanceScrapyFetcher()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+        
+        fetcher = nil
+    }
+    
+    func testElanceScrapyFetcher() {
+        // This is an example of a functional test case.
+        let expectation = expectationWithDescription("ElanceScrapyFetcher")
+        
+        fetcher?.fetchHtml({ (object, sucess) -> Void in
+            expectation.fulfill()
+        })
+        
+        
+        waitForExpectationsWithTimeout(10) { (error) in
+            XCTAssertNil(error, "\(error)")
+        }
+    }
+    
+    
+    
+}
