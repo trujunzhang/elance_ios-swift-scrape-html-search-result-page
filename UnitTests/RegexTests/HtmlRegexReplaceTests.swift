af690ed2d1d09a55af368ea798297f39df64c2d2
wanghaogithub720 <wanghaobackup@gmail.com>
2015-05-09 10:59:10 +0800

    use GData-html to parse html.

--- UnitTests/RegexTests/HtmlRegexReplaceTests.swift
+++ UnitTests/RegexTests/HtmlRegexReplaceTests.swift
@@ -11,22 +11,32 @@ import XCTest
 
 class HtmlRegexReplaceTests: XCTestCase {
 
+    let html = ""
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
 
+     func readFile(fileName: String) -> String {
+        let path = NSBundle.mainBundle().pathForResource(fileName, ofType: "html")
+//         println("\(path)")
+        var data = String(contentsOfFile: path!, encoding: NSUTF8StringEncoding, error: nil)
+
+        return data!
+    }
+
     func testExample() {
-//        let html =
+        let html = readFile("content")
+        println("\(html)")
         var rx = NSRegularExpression.rx("<span.*span>", options: .CaseInsensitive);
 //        let  xx =  html.replace(rx, with: html )
     }
-
 
 
 }
